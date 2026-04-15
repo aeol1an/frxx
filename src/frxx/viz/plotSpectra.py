579f2e32a6b9f1be64866f4f24486b92f65ae0ef
Ameya Naik <symaen123@gmail.com>
2026-04-15 16:24:42 -0500

    merge plotSpectra.py into lazy_compute

--- src/frxx/viz/plotSpectra.py
+++ src/frxx/viz/plotSpectra.py
@@ -14,7 +14,7 @@ from matplotlib.colors import Colormap
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import matplotlib.patheffects as pe
 
-from ..utils.coordConvert import beamHeightWithRadarHeight
+from ..misc.coordConvert import beamHeightWithRadarHeight
 
 cacheKey = None
 cachedHt = None
@@ -58,7 +58,7 @@ def plotRangeDoppler(
     axFrac = 0.75
     start = (1-axFrac)/2
     ax = fig.add_axes((start, start, axFrac, start+axFrac))
-    
+
     plot = ax.pcolormesh(
         velMS, rangesKM, data, 
         cmap=cmap, vmin=vmin, vmax=vmax
@@ -68,6 +68,11 @@ def plotRangeDoppler(
     ax.set_ylabel('Range (km)', size=4*lm, labelpad=0)
     ax.tick_params(axis='both', labelsize=4*lm, length=2, direction='in', pad=1)
     
+    xl, xr = ax.get_xlim()
+    cbarBuffer = 0.125
+    xr += (xr-xl)*cbarBuffer
+    ax.set_xlim((xl, xr))
+
     ax2 = ax.twinx()
     rTicks = np.array(ax.get_yticks(), dtype=np.float64)
     
@@ -91,8 +96,6 @@ def plotRangeDoppler(
         cachedHt = htTicks
     
     ax2.tick_params(axis='both', labelsize=4*lm, length=2, direction='in', pad=1)
-    if htTicks is None:
-        raise ValueError("Something went wrong with htTicks caching.")
     if np.mean(htTicks) < 0.1:
         ax2.set_yticks(rTicks, np.round(htTicks*1000, decimals=2))
         ax2.set_ylim(ax.get_ylim())
@@ -104,7 +107,7 @@ def plotRangeDoppler(
     
     textBorderWidth = 0.5*lm
     
-    cbarAx = inset_axes(ax, width=2.5*axFrac*0.1*lm, height=aspectRatio*width*0.83*(start+axFrac), loc='center right', borderpad=0.3)
+    cbarAx = ax.inset_axes(bounds=(0.875, 0.075, 0.1, 0.85))
     cb = fig.colorbar(
         plot,
         cax = cbarAx,
