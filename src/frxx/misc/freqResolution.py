52fae47a83828969124a47e523e7a6e3f1e91b14
Ameya Naik <symaen123@gmail.com>
2026-07-20 17:14:49 -0700

    update freqResolution

--- src/frxx/misc/freqResolution.py
+++ src/frxx/misc/freqResolution.py
@@ -9,3 +9,8 @@ def velResolutionTonPulses(delta_v, prf = 4000, wavelength = 0.0308):
     delta_fd = delta_v * 2.0 / wavelength
     nPulses = prf / delta_fd
     return nPulses
+
+def velSpanToNumBins(delta_v, nFFT, prf=4000, wavelength=0.0308):
+    bin_width = prf * wavelength / (2.0 * nFFT)   # m/s per bin
+    nBins = int(round(delta_v / bin_width))
+    return max(1, nBins)  # a window should be at least 1 bin wide
